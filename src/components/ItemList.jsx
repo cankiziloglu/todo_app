d4fe8f004467845beeb3ff5ed4f1fe5d3664ce55
Can Gokhan Kiziloglu <can.kiziloglu@gmail.com>
2024-02-20 11:52:25 +0300

    added sorting to task list

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,12 +1,39 @@
-import EmptyView from "./EmptyView";
+import { useState } from 'react';
+import EmptyView from './EmptyView';
+import Select from 'react-select';
 
 const ItemList = ({ items, handleRemoveItem, handleToggleItem }) => {
+  const sortingOptions = [
+    { value: 'default', label: 'Sort by default' },
+    { value: 'completed', label: 'Sort by completed' },
+    { value: 'uncompleted', label: 'Sort by uncompleted' },
+  ];
+
+  const [sortBy, setSortBy] = useState('default');
+
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === 'completed') {
+      return b.completed - a.completed;
+    }
+    if (sortBy === 'uncompleted') {
+      return a.completed - b.completed;
+    }
+    return;
+  });
+
   return (
     <ul className='item-list'>
-      {
-        items.length === 0 && <EmptyView />
-      }
-      {items.map((item) => (
+      {items.length === 0 && <EmptyView />}
+      {items.length > 0 && (
+        <section className='sorting'>
+          <Select
+            options={sortingOptions}
+            defaultValue={sortingOptions[0]}
+            onChange={(option) => setSortBy(option.value)}
+          />
+        </section>
+      )}
+      {sortedItems.map((item) => (
         <li key={item.id} className='item'>
           <label>
             <input
